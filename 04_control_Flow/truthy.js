5e8e4e00c35086e0f4b83af4f33633035db95c3d
Rohit kumar <rohit.rksaini28@gmail.com>
2024-09-19 17:02:56 +0000

    ??

---
+++ 04_control_Flow/truthy.js
@@ -0,0 +1,10 @@
+let vel1;
+//vel1 = 5 ?? 10
+//vel1 = null ?? 25
+
+console.log(vel1)
+
+// terniary operator
+
+const price = 100
+price <= 80 ? console.log("less then 80") : console.log("more then 80")
